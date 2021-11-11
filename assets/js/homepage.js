bad89b51abe84e1a1f49f0492aaa1c4774269e7d
Woogster <john.m.wooley@gmail.com>
2021-11-11 17:56:29 -0600

    Working on user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var getUserRepos = function() {
     fetch("https://api.github.com/users/octocat/repos");
+    var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
 };
 
 getUserRepos();
